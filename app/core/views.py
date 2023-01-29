81b0fdc0fb98cd693d4070e94af48afd8c89be54
tiger <tiger@tiger.tiger>
2023-01-30 00:46:08 +0300

    added new custom permession.

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.decorators import action
@@ -5,22 +6,25 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from .models import *
 from .serializers import ChefSerializer
+from .permissions import IsCreatorOrReadOnly
 
 
 class ChefViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Chef.objects.all()
     serializer_class = ChefSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsCreatorOrReadOnly]
     
     def get_permissions(self):
         """Will allow anyone to see the chefs but only authenticated can update """
         if self.request.method == 'GET':
             return [AllowAny()]
-        return [IsAuthenticated()]
+        return [IsCreatorOrReadOnly()]
 
     
     @action(detail=False, methods=['GET', 'PUT'])
     def me(self, request):
+        if not request.user.is_authenticated:
+            return Response(status.HTTP_404_NOT_FOUND)
         (chef, created) = Chef.objects.get_or_create(user_id=request.user.id)
         if request.method == 'GET':
             serializer = ChefSerializer(chef)
