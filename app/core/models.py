d63481994abbc9a05786624a0ae17897a982b1d8
tiger <tiger@tiger.tiger>
2023-01-31 16:30:48 +0300

    fixes

--- app/core/models.py
+++ app/core/models.py
@@ -28,8 +28,7 @@ class Chef(models.Model):
         
         
 class Ingredient(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    
+    name = models.CharField(max_length=255, unique=True)  
+    chef = models.ForeignKey(Chef, on_delete=models.SET_NULL, null=True)  
     def __str__(self):
         return self.name
