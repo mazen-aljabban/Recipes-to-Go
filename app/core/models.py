aed44e5416d4e55ff1622d99cf10b2af93243c27
tiger <tiger@tiger.tiger>
2023-02-18 18:40:01 +0300

    added signal for creating chef when user is created, and some fixes.

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.db import models
 from uuid import uuid4
-from users.models import User
 
 # Create your models here.
 
